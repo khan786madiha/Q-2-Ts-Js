0a5bd5d83139e2262b63d090c41080ea118daaf7
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-02-23 03:06:59 -0800

    Q-02 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-02

---
+++ Q2.js
@@ -0,0 +1,8 @@
+"use strict";
+// 2.Personal message: store a person's name  in a variable, and print a message that person. 
+// Your message should be simple, such as,"Hellow Eeric,"would you like to learn some python today?" `); 
+// that personalbar.visible
+let firstName = "Eric";
+console.log(`"Hellow ${firstName}, would you like to learn some python today?" `);
+let name1 = "AYESHA";
+console.log(`"HEY ${name1}, how are you doing?"`);
---
+++ Q2.ts
@@ -0,0 +1,12 @@
+// 2.	Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+// Storing the person's name in a variable
+let personName: string = "Eric";
+
+// Printing a personalized message
+console.log(`"Hellow ${personName}, would you like to learn some python today?" `);  
+
+
+let name1: string = "AYESHA";
+
+console.log(`"HEY ${name1}, how are you doing?"`);
